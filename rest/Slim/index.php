dded3ac1d97e17e8b12f53187610b8c141dcea5b
Nikhil R <rnikhil2751996@gmail.com>
2015-02-26 05:25:23 +0530

    Add login_client code

--- rest/Slim/index.php
+++ rest/Slim/index.php
@@ -1,10 +1,10 @@
 <?php
 require 'Slim/Slim.php';
-require 'RedBean/rb.php';
+// require 'RedBean/rb.php';
 \Slim\Slim::registerAutoloader();
 session_cache_limiter(false);
 session_start();
-R::setup('mysql:host=localhost;dbname=instaconsult','root','');
+// R::setup('mysql:host=localhost;dbname=instaconsult','root','');
 // R::freeze(true);
 
 $app = new \Slim\Slim();                    // pass an associative array to this if you want to configure the settings
@@ -15,6 +15,22 @@ $app->post('/login', function () use ($app) {
 $result=  json_decode($body);
 $username=$result->username;
 $password=$result->password;
+
+
+$connection = mysqli_connect("localhost", "root", "", "instaconsult");
+$result = mysqli_query($connection, "select * from login_client where password='$password' AND username='$username'");
+
+$rows = mysqli_num_rows($result);
+
+if ($rows == 1) {
+  $_SESSION['login_client']=$username; // Initializing Session(setting the session variable)
+  header("location: profile.php"); // Redirecting To Other Page
+}
+
+else {
+  $error = "Username or Password is invalid";
+}
+mysqli_close($connection);
 
 
   $app->response()->header('Content-Type', 'application/json');
