cb5a42be67bdf1e2658d918f165f8d54f7e60fc3
Nikhil R <rnikhil2751996@gmail.com>
2015-02-26 04:55:14 +0530

    Start from beginning

--- rest/Slim/index.php
+++ rest/Slim/index.php
@@ -2,35 +2,15 @@
 require 'Slim/Slim.php';
 require 'RedBean/rb.php';
 \Slim\Slim::registerAutoloader();
-// session_cache_limiter(false);
-// session_start();
+session_cache_limiter(false);
+session_start();
 $app = new \Slim\Slim();                    // pass an associative array to this if you want to configure the settings
 
 $app->post('/login', function () use ($app) {
 
-
-  $username = $app->request->post('username');
-  $password = $app->request->post('password');
-
-
-      echo json_encode($username);
-      echo json_encode($password);
-
-      $connection = mysqli_connect("localhost", "root", "", "instaconsult");
-      $result = mysqli_query($connection, "select * from login_client where password='$password' AND username='$username'");
-
-      $rows = mysqli_num_rows($result);
-
-      if ($rows == 1) {
-        $_SESSION['login_client']=$username; // Initializing Session(setting the session variable)
-        echo json_encode("logged in"); // Redirecting To Other Page
-      }
-
-      else {
-        echo json_encode("Username or Password is invalid");
-      }
-      mysqli_close($connection); // Closing Connection
-
+  $request = $app->request();
+  $body = $request->getBody();
+  $input = json_decode($body);
 
 
   $app->response()->header('Content-Type', 'application/json');
