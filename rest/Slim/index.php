38bfef4a963408be262bd3bb6e496f17c002f0da
Nikhil R <rnikhil2751996@gmail.com>
2015-02-25 21:56:10 +0530

    Edit Slim

--- rest/Slim/index.php
+++ rest/Slim/index.php
@@ -2,7 +2,10 @@
 require 'Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
-$app = new \Slim\Slim(); // pass an associative array to this if you want to configure the settings
+
+
+
+$app = new \Slim\Slim();                 // pass an associative array to this if you want to configure the settings
 
 
 
