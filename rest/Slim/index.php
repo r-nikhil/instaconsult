0be186994c21a0a984eb211a78cf85c63da57446
Nikhil R <rnikhil2751996@gmail.com>
2015-02-26 04:56:20 +0530

    Database conenction

--- rest/Slim/index.php
+++ rest/Slim/index.php
@@ -4,6 +4,9 @@ require 'RedBean/rb.php';
 \Slim\Slim::registerAutoloader();
 session_cache_limiter(false);
 session_start();
+R::setup('mysql:host=localhost;dbname=appdata','root','');
+R::freeze(true);
+
 $app = new \Slim\Slim();                    // pass an associative array to this if you want to configure the settings
 
 $app->post('/login', function () use ($app) {
