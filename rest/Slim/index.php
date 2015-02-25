36b2607f88a3fcccff352cd2baa3fd465ab3fcc4
Nikhil R <rnikhil2751996@gmail.com>
2015-02-26 05:18:37 +0530

    Working copy. Check this to revert

--- rest/Slim/index.php
+++ rest/Slim/index.php
@@ -4,20 +4,20 @@ require 'RedBean/rb.php';
 \Slim\Slim::registerAutoloader();
 session_cache_limiter(false);
 session_start();
-R::setup('mysql:host=localhost;dbname=appdata','root','');
-R::freeze(true);
+R::setup('mysql:host=localhost;dbname=instaconsult','root','');
+// R::freeze(true);
 
 $app = new \Slim\Slim();                    // pass an associative array to this if you want to configure the settings
 
 $app->post('/login', function () use ($app) {
 
-  $request = $app->request();
-  $body = $request->getBody();
-  $input = json_decode($body);
+  $body = $app->request->getBody();
+$result=  json_decode($body);
+$username=$result->username;
+$password=$result->password;
 
 
   $app->response()->header('Content-Type', 'application/json');
-
 });
 
 $app->run();
