21244af4d4c6abefbfdd0ce9857c48276df3873d
Nikhil R <rnikhil2751996@gmail.com>
2015-03-07 20:37:28 +0530

    Added PHP scripts for Signup also

--- instaconsult-front/process1.php
+++ instaconsult-front/process1.php
@@ -1,9 +1,20 @@
 <?php
+include "connection.php";
 
+$first_name= $_POST["first_name"];
+$last_name= $_POST["last_name"];
+$email_id=$_POST["email_id"];
+$password=$_POST["password"];
 
+$sql = "INSERT INTO signup (first_name, last_name, email, password)
+VALUES ('$first_name', 'last_name', '$email_id','$password')";
+if (mysqli_query($connection, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
 
-
-
+mysqli_close($connection);
 
 
 ?>
